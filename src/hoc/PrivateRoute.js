7fffe8d3f771b8dc152e542bc6b639e53ce15273
hoomanh1998 <hoomanh1998@gmail.com>
2020-07-15 03:53:01 +0430

    new file

---
+++ src/hoc/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+    let isLogged = null;
+    if (JSON.parse(localStorage.getItem('user'))) {
+        isLogged = JSON.parse(localStorage.getItem('user')).isLogged;
+    }
+
+    return (
+        <Route exact {...rest} render={(props) => (
+            isLogged === true
+              ? <Component {...props} />
+              : <Redirect to='/login' />
+          )} />
+    );
+}
+
+export default PrivateRoute;
