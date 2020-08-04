f25715557d4fa3ab2b6f4aa070aed994b6a4eb75
hoomanh1998 <hoomanh1998@gmail.com>
2020-08-04 18:33:21 +0430

    file modified

--- src/store/store.js
+++ src/store/store.js
@@ -3,19 +3,21 @@ import reducer, { initialState } from './reducer';
 
 export const Store = createContext();
 
-const applyThunk = dispatch => action => {
+const Thunk = dispatch => action => {
   if (typeof action === 'function')
+    //this will pass dispatch function as an argument to the action function in actions.js and it will dispatch another actions inside that action function.
     return action(dispatch)
+  //this will dispatch an action with regular action with type & payload.
   return dispatch(action)
 }
 
-const useThunkReact = (reducer, initialState) => {
+const useThunk = (reducer, initialState) => {
   const [state, dispatch] = useReducer(reducer, initialState)
-  return [state, applyThunk(dispatch)]
+  return [state, Thunk(dispatch)]
 }
 
 const Provider = (({ children }) => {
-  const [state, dispatch] = useThunkReact(reducer, initialState);
+  const [state, dispatch] = useThunk(reducer, initialState);
   return <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
 })
 
